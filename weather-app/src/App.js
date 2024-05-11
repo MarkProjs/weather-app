1e7172c20191152bcbd58a76ed1e41cc633df6e3
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2024-05-11 19:28:12 -0400

    adding and testing the fetch funtion

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,32 +1,50 @@
 import { useState, useEffect } from "react";
-import fetchData from "./controllers/FetchModule";
-import SearchBar from "./components/SearchBar";
 
 function App() {
-  const [searchResult, setSearchResult] = useState(null);
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [temp, setTemp] = useState(0.0);
+  const [desc, setDesc] = useState("");
 
-  async function handleSearch(query) {
-    const result = await fetchData(query)
-    setSearchResult(result);
-    console.log(`Performing search for: ${query}`);
-    
+  async function fetchWeatherData() {
+    try {
+      setIsLoading(true);
+      const resp = await fetch(
+        `https://api.weatherbit.io/v2.0/current?city=${city}&key=${process.env.REACT_APP_API_KEY}`
+      );
+      const weatherData = await resp.json();
+      setData(weatherData);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    } finally {
+      setIsLoading(false);
+    }
   }
+
   useEffect(()=> {
-    console.log(searchResult);
-  }, [searchResult]);
-  return (
-    <div className="App">
-      <div>
-      <h1>Search Example</h1>
-        <SearchBar onSearch={handleSearch} />
-          {/* {searchResult !== null ? (
-            <p>{searchResult}</p>
-          ) : (
-            <p>No results found</p>
-          )} */}
+    if (city) {
+      fetchWeatherData();   
+    }
+  }, [city]);
+
+  function handleCityChange(evt) {
+    setCity(evt.target.value);
+  }
+  return <div className="App">
+    <input type="text" value={city} placeholder="Search..." onChange={handleCityChange}/>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : data ? (
+        <div>
+          <h2>Weather in {city}</h2>
+          <p>{/* Display weather data here */}</p>
         </div>
-    </div>
-  );
+      ) : (
+        <p>Please enter a city</p>
+      )}
+
+  </div>;
 }
 
 export default App;
